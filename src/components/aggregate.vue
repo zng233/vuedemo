9daa2c93060f8dce843890678e4b652dea87978a
zng233 <3207782856@qq.com>
2025-06-12 19:42:22 +0800

    将Carousel,Menubar,hot,last四个组件打包进aggregate.vue中

---
+++ src/components/aggregate.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import last from '@/components/last.vue'
+import Menubar from "@/components/menubar.vue";
+import Hot from "@/components/hot.vue";
+import Carousel from "@/components/Carousel.vue";
+</script>
+
+<template>
+  <div>
+    <Carousel/>
+    <Menubar/>
+    <hot/>
+    <last/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
